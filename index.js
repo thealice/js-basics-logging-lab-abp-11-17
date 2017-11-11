1455f81a21ddb2f3c951c12911ab53130e1b87ec
Alice Freda <alicefreda@gmail.com>
2017-11-11 23:49:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const driver = 'bob';
 const message = ', is the driver variable name';
 console.log('this code was called');
 console.log(`${driver}`);
-console.log([`${driver}`,', is the driver variable name']);
+console.log([`${driver}`,`${message}`]);
